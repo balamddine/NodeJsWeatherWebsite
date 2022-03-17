3ace3e8205a0c949fc56fec482ef89014f88a475
balamddine <balamddine@gmail.com>
2022-03-17 16:49:01 +0200

    init commit

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,3 @@
-console.log("Hello")
     // this function is used in all modern browser 
     //on client side javascript ( it will not run in server side nodejs application)
     // fetch('http://puzzle.mead.io/puzzle').then((response) => {
@@ -9,26 +8,26 @@ console.log("Hello")
 
 
 
-const weatherForm = document.querySelector('form');
-const search = document.querySelector('input');
+    const weatherForm = document.querySelector('form');
+    const search = document.querySelector('input');
 
-const messageOne = document.querySelector('#message-1');
-const messageTwo = document.querySelector('#message-2');
+    const messageOne = document.querySelector('#message-1');
+    const messageTwo = document.querySelector('#message-2');
 
-weatherForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    messageOne.textContent = 'Loading...';
-    messageTwo.textContent = '';
-    const location = search.value;
-    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
-        response.json().then((data) => {
-            if (data.error) {
-                messageOne.textContent = data.error
-            } else {
-                messageOne.textContent = data.location
-                messageTwo.textContent = data.forecast
-            }
+    weatherForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        messageOne.textContent = 'Loading...';
+        messageTwo.textContent = '';
+        const location = search.value;
+        fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+            response.json().then((data) => {
+                if (data.error) {
+                    messageOne.textContent = data.error
+                } else {
+                    messageOne.textContent = data.location
+                    messageTwo.textContent = data.forecast
+                }
 
+            })
         })
     })
-})
